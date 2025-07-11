4e7d8f38690e1503db2bdcb32167e5f8dec040ed
hmasami <hisayama@oec.co.jp>
2025-07-11 22:44:32 +0900

    feat:動いたぞー
    closes #27

--- pomoapp-backend/app/api/dashboard.py
+++ pomoapp-backend/app/api/dashboard.py
@@ -1,5 +1,3 @@
-# app/api/dashboard.py
-
 from fastapi import APIRouter
 from pydantic import BaseModel
 import google.generativeai as genai
@@ -7,10 +5,16 @@ import os
 import time
 from dotenv import load_dotenv
 
+# .env を読み込む
+load_dotenv()
+
 router = APIRouter()
 
-# Google Gemini APIキーを環境変数から取得
-GOOGLE_API_KEY = "Gitにあげるのはちょっと..."
+# 環境変数からAPIキーを取得
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+print("DEBUG: 現在のディレクトリ:", os.getcwd())
+print("DEBUG: GOOGLE_API_KEY:", os.getenv("GOOGLE_API_KEY"))
 
 # Gemini APIに接続設定
 genai.configure(api_key=GOOGLE_API_KEY)
@@ -20,12 +24,12 @@ class WordRequest(BaseModel):
 
 @router.post("/word")
 async def receive_word(request: WordRequest):
-    prompt = f"以下の単語について面白く返して: {request.word}"
+    prompt = f"以下の単語について10文字以下の単語で面白く返して: {request.word}"
     print("GOOGLE_API_KEY:", GOOGLE_API_KEY)
 
     try:
         print("Geminiプロンプト送信中...")
-        model = genai.GenerativeModel("gemini-pro")
+        model = genai.GenerativeModel('models/gemini-1.5-flash-latest')
         response = model.generate_content(prompt)
         print("Gemini応答取得完了")
         answer = response.text
